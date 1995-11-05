aa3986cef5be5d252340f4328f6e6333d2fb3c58
houghton <>
1995-11-05 13:05:08 +0000

    Initial regex config header

---
+++ src/arch/AIX32/_GnuRegexCfg.h
@@ -0,0 +1,17 @@
+#ifndef _regexcfg_h_
+#define _regexcfg_h_
+
+/* regex.c is from gnu grep 2.0
+ * These are the defineds needed for this platform
+ */
+
+
+#define STDC_HEADERS     1 
+#define HAVE_STRING_H    1 
+#define HAVE_SYS_PARAM_H 1 
+#define HAVE_UNISTD_H    1 
+#define HAVE_GETPAGESIZE 1 
+#define HAVE_MEMCHR      1 
+#define HAVE_STRERROR    1 
+
+#endif
