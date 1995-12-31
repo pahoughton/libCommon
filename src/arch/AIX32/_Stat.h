2770f7da4ee0ce9694dfae2d0f6cb8d3c5c07fb0
houghton <>
1995-12-31 11:28:47 +0000

    Initial Version.

---
+++ src/arch/AIX32/_Stat.h
@@ -0,0 +1,45 @@
+#ifndef __Stat_h_
+#define __Stat_h_
+/*********************************************************************
+ *
+ * Title:            _Stat.h
+ *
+ * Description:
+ *
+ *
+ *
+ * Notes:
+ *
+ * Programmer:	    Paul Houghton x2309 - (paul_houghton@wiltel.com)
+ *
+ * Start Date:	    12/09/95 07:06
+ *
+ * Modification History:
+ *
+ * $Log$
+ *
+ *********************************************************************/
+
+#if !defined( _ALL_SOURCE )
+#define _ALL_SOURCE 1
+#endif
+
+#if !defined( _XOPEN_SOURCE )
+#define _XOPEN_SOURCE 1
+#endif
+
+#if !defined( _POSIX_SOURCE )
+#define _POSIX_SOURCE 1
+#endif
+
+#if !defined( _ANSI_C_SOURCE )
+#define _ANSI_C_SOURCE 1
+#endif
+
+#include "_Common.h"
+
+#include <sys/mode.h>
+#include <sys/stat.h>
+
+#endif /* ! def __Stat_h_ */
+
