063b5fd0d10b7f212ce576f1c92b22b709227dde
houghton <>
1996-02-29 16:22:17 +0000

    Initail version

---
+++ src/arch/Linux/_Stat.h
@@ -0,0 +1,31 @@
+#ifndef __Stat_h_
+#define __Stat_h_
+/*********************************************************************
+ *
+ * Title:            _Stat.h
+ *
+ * Description:
+ *
+ *
+ *
+ * Notes:
+ *
+ * Programmer:	    Paul Houghton x2309 - (paul_houghton@wiltel.com)
+ *
+ * Start Date:	    12/31/95 05:06
+ *
+ * Modification History:
+ *
+ * $Log$
+ * Revision 2.1  1995/12/31 11:28:47  houghton
+ * Initial Version.
+ *
+ *
+ *********************************************************************/
+
+#include "_Common.h"
+
+#include <sys/stat.h>
+
+#endif /* ! def __Stat_h_ */
+
