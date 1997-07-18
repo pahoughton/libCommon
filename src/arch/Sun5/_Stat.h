fb523ba0e9d6d8c7f0e163d4a88d59fa53be1ea0
houghton <>
1997-07-18 18:47:38 +0000

    Initial Version.

---
+++ src/arch/Sun5/_Stat.h
@@ -0,0 +1,28 @@
+#ifndef __Stat_h_
+#define __Stat_h_
+/*********************************************************************
+ *
+ * Title:            _Stat.h
+ *
+ * Description:
+ *
+ *
+ *
+ * Notes:
+ *
+ * Programmer:	    Paul Houghton x2309 - (paul_houghton@wiltel.com)
+ *
+ * Start Date:	    07/17/97 08:36
+ *
+ * Modification History:
+ *
+ * $Log$
+ *
+ *********************************************************************/
+
+#include "_Common.h"
+
+#include <sys/stat.h>
+
+#endif /* ! def __Stat_h_ */
+
