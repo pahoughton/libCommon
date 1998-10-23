d8362229a1e55aa42fde9dec6edeb2bcdfc92b58
houghton <>
1998-10-23 11:38:02 +0000

    Initial Version.

---
+++ src/arch/Sun5/TestConfig.h
@@ -0,0 +1,51 @@
+#ifndef _TestConfig_h_
+#define _TestConfig_h_
+/*********************************************************************
+ *
+ * Title:       TestConfig.h
+ * Project:	Common
+ * Desc:
+ *
+ *  
+ *
+ * Notes:
+ *
+ * Author:	Paul A. Houghton - (paul.houghton@wcom.com)
+ * Created:	09/23/98 05:38
+ *
+ * Revision History: (See end of file for Revision Log)
+ *
+ *  Last Mod By:    $Author$
+ *  Last Mod:	    $Date$
+ *  Version:	    $Revision$
+ *
+ * $Id$
+ *
+ *********************************************************************/
+
+#include <CommonConfig.h>
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+
+
+
+
+#if defined( __cplusplus )
+};
+#endif
+
+/*
+ *
+ * Revision Log:
+ *
+ * $Log$
+ * Revision 1.1  1998/09/24 15:28:00  houghton
+ * Initial Version.
+ *
+ *
+ */
+
+#endif /* ! def _TestConfig_h_ */
